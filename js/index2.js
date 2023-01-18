0a39830959e565ab8d72436b1137d86d3eacfd2c
yjlee9909 <yjlee9909@naver.com>
2023-01-18 15:31:23 +0900

    카트 아이템 추가하기

--- js/index2.js
+++ js/index2.js
@@ -48,7 +48,48 @@ const resData = (productData) => {
   itemDetail = [...document.querySelectorAll(".btn-item")];
   // console.log([...itemDetail]);
 };
-// console.log(itemDetail);
+
+// 입금액 입력 함수
+const depositFunc = (e) => {
+  // 입금액
+  const depositValue = inpMoney.value;
+  // 소지금
+  const intMyMoney = parseInt(txtMyMoney.textContent.replaceAll(",", ""));
+  // 잔액
+  const intReMoney = parseInt(returnMoney.textContent.replaceAll(",", ""));
+
+  if (depositValue) {
+    if (depositValue <= intMyMoney && depositValue > 0) {
+      // 잔액 표시하기
+      returnMoney.textContent =
+        Number(
+          parseInt(intReMoney ? intReMoney : 0) + parseInt(depositValue)
+        ).toLocaleString() + " 원";
+
+      // 소지금 액수 차감
+      txtMyMoney.textContent =
+        (intMyMoney - depositValue).toLocaleString() + " 원";
+    } else {
+      alert("소지금이 부족합니다.");
+    }
+    inpMoney.value = null;
+  }
+};
+
+// 거스름돈 반환 함수
+const retMoneyFunc = (e) => {
+  console.log("click2");
+  // 잔액
+  const intReMoney = parseInt(returnMoney.textContent.replaceAll(",", ""));
+  // 소지금
+  const intMyMoney = parseInt(txtMyMoney.textContent.replaceAll(",", ""));
+  e.preventDefault();
+  // 잔액 초기화
+  if (intReMoney) {
+    txtMyMoney.textContent = (intReMoney + intMyMoney).toLocaleString() + " 원";
+    returnMoney.textContent = "원";
+  }
+};
 
 // 데이터 받아오는 함수 실행
 getData()
@@ -58,4 +99,49 @@ getData()
   })
   .then((data) => {
     console.log(data);
+
+    // 입금액 입력 함수
+    btnPut.addEventListener("click", depositFunc);
+
+    // 거스름돈 반환 함수
+    btnReturn.addEventListener("click", retMoneyFunc);
+
+    // 음료수 선택
+    // console.log(itemDetail);
+    itemDetail.forEach((item) => {
+      item.addEventListener("click", (event) => {
+        console.log(event.currentTarget);
+        // 잔액
+        const intReMoney = parseInt(
+          returnMoney.textContent.replaceAll(",", "")
+        );
+        const curTargetEl = event.currentTarget;
+        let isStaged = false; // 이미 선택
+        const curTargetElPrice = parseInt(curTargetEl.dataset.cost);
+        const stagedListItem = stagedList.querySelectorAll("li");
+
+        // 잔액이 있을 경우 음료수 선택 가능
+        if (intReMoney >= curTargetElPrice) {
+          // 카트 리스트 생성 함수
+
+          // 선택했다면 리스트 생성하기
+          if (!isStaged) {
+            const stagedItem = document.createElement("li");
+            stagedItem.classList = "drink";
+            stagedItem.dataset.cost = item.dataset.cost;
+            stagedItem.dataset.name = item.dataset.name;
+            stagedItem.dataset.count = item.dataset.count;
+            stagedItem.dataset.img = item.dataset.img;
+            stagedItem.innerHTML = `
+            <img src="./images/${item.dataset.img}" alt="">
+            <p>${item.dataset.name}</p>
+            <span class="get-num">2</span>
+            `;
+            stagedList.appendChild(stagedItem);
+          }
+        } else if (intReMoney < curTargetElPrice) {
+          alert("잔액이 부족합니다.");
+        }
+      });
+    });
   });

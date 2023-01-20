4d67e7096706eafec84bb915183b0424eee9a199
yjlee9909 <yjlee9909@naver.com>
2023-01-20 14:46:59 +0900

    fix : dataset 커스텀 속성 수정 및 카트 리스트 생성

--- js/index2.js
+++ js/index2.js
@@ -36,7 +36,7 @@ const resData = (productData) => {
     stagedItem.dataset.count = item.count;
     stagedItem.dataset.img = item.img;
     stagedItem.innerHTML = `
-    <button type='button' class='btn-item' data-item="${item.name}" data-count="${item.count}" data-price="${item.cost}" data-img="${item.img}">
+    <button type='button' class='btn-item' data-item="${item.name}" data-count="${item.count}" data-cost="${item.cost}" data-img="${item.img}">
       <img src="./images/${item.img}" alt=${item.name}>
       <p>${item.name}</p>
       <span class='price-btn'>${item.cost}원</span>
@@ -91,6 +91,21 @@ const retMoneyFunc = (e) => {
   }
 };
 
+// 선택한 아이템 담는 함수
+const addCartList = (target) => {
+  const stagedItem = document.createElement("li");
+  stagedItem.dataset.item = target.dataset.item;
+  stagedItem.dataset.price = target.dataset.price;
+  stagedItem.innerHTML = `
+    <button type="button" class="drink btn-staged">
+      <img src="./images/${target.dataset.img}" alt="" class="img-item">
+      <p class="txt-item">${target.dataset.item}</p>
+      <span class="get-num">1</span>
+    </button>
+  `;
+  stagedList.appendChild(stagedItem);
+};
+
 // 데이터 받아오는 함수 실행
 getData()
   .then((data) => {
@@ -108,6 +123,7 @@ getData()
 
     // 음료수 선택
     // console.log(itemDetail);
+
     itemDetail.forEach((item) => {
       item.addEventListener("click", (event) => {
         console.log(event.currentTarget);
@@ -115,32 +131,36 @@ getData()
         const intReMoney = parseInt(
           returnMoney.textContent.replaceAll(",", "")
         );
+        console.log(intReMoney);
         const curTargetEl = event.currentTarget;
+        console.log(curTargetEl.dataset.cost);
         let isStaged = false; // 이미 선택
         const curTargetElPrice = parseInt(curTargetEl.dataset.cost);
         const stagedListItem = stagedList.querySelectorAll("li");
 
-        // 잔액이 있을 경우 음료수 선택 가능
+        // 입금된 금액이 읍료수 값보다 많을 경우
         if (intReMoney >= curTargetElPrice) {
-          // 카트 리스트 생성 함수
+          returnMoney.textContent =
+            new Intl.NumberFormat().format(intReMoney - curTargetElPrice) +
+            " 원";
 
-          // 선택했다면 리스트 생성하기
-          if (!isStaged) {
-            const stagedItem = document.createElement("li");
-            stagedItem.classList = "drink";
-            stagedItem.dataset.cost = item.dataset.cost;
-            stagedItem.dataset.name = item.dataset.name;
-            stagedItem.dataset.count = item.dataset.count;
-            stagedItem.dataset.img = item.dataset.img;
-            stagedItem.innerHTML = `
-            <img src="./images/${item.dataset.img}" alt="">
-            <p>${item.dataset.name}</p>
-            <span class="get-num">2</span>
-            `;
-            stagedList.appendChild(stagedItem);
+          // forEach 문을 사용할 경우 반복의 종료가 불가능하다(return, break 작동 안함). 모든 원소를 순환할 필요가 없다면 비효율적.
+          for (const item of stagedListItem) {
+            // 클릭한 음료수가 이미 선택한 아이템인지 탐색
+            if (item.dataset.item === curTargetEl.dataset.item) {
+              item.querySelector(".get-num").textContent++;
+              isStaged = true;
+              break;
+            }
           }
-        } else if (intReMoney < curTargetElPrice) {
-          alert("잔액이 부족합니다.");
+
+          // 처음 선택한 경우
+          if (!isStaged) {
+            addCartList(curTargetEl);
+          }
+          curTargetEl.dataset.count--;
+        } else {
+          alert("잔액이 부족합니다. 돈을 입금해주세요");
         }
       });
     });
